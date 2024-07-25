acfb0002ff5073893f671253a1448a78fa45f723
arc-research-analytics <wwright@atlantaregional.org>
2024-07-25 10:58:55 -0400

    added pixel screen height

--- responsive.py
+++ responsive.py
@@ -11,6 +11,8 @@ st.set_page_config(
 )
 
 # get the screen height to set the heights of the map and line charts
-screen_width = streamlit_js_eval(js_expressions='screen.width', key = 'SCR')
+screen_width = streamlit_js_eval(js_expressions='screen.width', key = 'SCR_1')
+screen_height = streamlit_js_eval(js_expressions='screen.height', key = 'SCR_2')
 
 st.write(f'This screen is {screen_width} pixels wide!')
+st.write(f'And also {screen_height} pixels high!')
